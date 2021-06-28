cc4242dc70634ec4da693a29f3241fa824f8eda6
Dainyleen Masangkay <dainyleen@gmail.com>
2021-06-28 20:48:54 +1200

    Created a for loop for the list

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-// Create showPage function
+// Create the 'showPage' function
 // This function will create and insert/append the elements needed to display a "page" of nine students
 
 function showPage(list, page) {
    const itemsPerPage = 9
    const startIndex = (page * itemsPerPage) - itemsPerPage
    const endIndex = (page * itemsPerPage)
-
    const studentList = document.querySelector('.student-list')
    studentList.innerHTML = ''
+
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+
+      }
+   }
 }
 
 showPage(data, 1)
