70169ed91fc56de50418e846e54a8ec76c449ca0
Dainyleen Masangkay <dainyleen@gmail.com>
2021-06-28 20:44:08 +1200

    Created studentList variable

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+// Create showPage function
 // This function will create and insert/append the elements needed to display a "page" of nine students
 
 function showPage(list, page) {
    const itemsPerPage = 9
    const startIndex = (page * itemsPerPage) - itemsPerPage
    const endIndex = (page * itemsPerPage)
+
+   const studentList = document.querySelector('.student-list')
+   studentList.innerHTML = ''
 }
 
 showPage(data, 1)
